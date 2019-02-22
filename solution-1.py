b477015fb45bcac383107549757a4397c30fb6a1
Niamh O'Leary <47368612+NiamhOL@users.noreply.github.com>
2019-02-22 14:37:25 +0000

    Update solution-1.py

--- solution-1.py
+++ solution-1.py
@@ -1,6 +1,13 @@
 # Solution to problem 1
+# Name: Niamh O'Leary
+# Student ID: G00376339
+# Last date modified: 22 Feb 2019
 
-n = int(input("Enter a positive number:"))
+# Write a program that asks the user to input any positive integer and outputs the sum of all numbers between one and that number
+
+n = int(input("Enter a positive number:")) 
 sum_num = (n * (n+1))/2
 print (sum_num)
+
+Print ("I just wrote my first program")
 
