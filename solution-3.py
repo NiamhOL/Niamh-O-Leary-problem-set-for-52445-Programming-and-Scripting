75a7f19c09196a91e279f77c6ef96b4b1cce135b
Niamh O'Leary <47368612+NiamhOL@users.noreply.github.com>
2019-02-25 19:58:25 +0000

    Add files via upload

--- solution-3.py
+++ solution-3.py
@@ -1,2 +1,12 @@
 # Solution to problem 3
 
+# Niamh O'Leary
+# ID: G00376339
+
+#Write a program that prints all numbers between 1,000 and 10,00 that are divisible by 6 but not 12.
+
+for num in range (1000, 10000):
+    if num % 6 == 0 and num % 12 != 0
+    print (num)
+
+    
