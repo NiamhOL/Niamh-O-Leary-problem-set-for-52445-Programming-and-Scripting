3f1de0fed4a2095b45641b882e5ff70ffa6bd9ca
Niamh O'Leary <47368612+NiamhOL@users.noreply.github.com>
2019-03-10 16:09:17 +0000

    Add files via upload

---
+++ solution-7.py
@@ -0,0 +1,15 @@
+# Solution to problem 7 #
+
+#Student: Niamh O'Leary#
+
+#ID: G00376339#
+
+#Date: 10/03/2019#
+
+#Write a program that takes a positive floating point number as input and outputs an approximation of its square root#
+
+#Note: for the problem please use number 14.5. 
+num = 14.5 
+
+num_sqrt = num ** 0.5 #calulates the square root#
+print("The square root of %0.3f is %0.3f"%(num, num_sqrt)) #prints the original number and its square root#
