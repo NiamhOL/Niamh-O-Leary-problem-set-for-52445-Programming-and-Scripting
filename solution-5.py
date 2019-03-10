0532bef3a2edaae8d95cf6edea8799e3234a1a69
Niamh O'Leary <47368612+NiamhOL@users.noreply.github.com>
2019-03-10 14:15:33 +0000

    Add files via upload

---
+++ solution-5.py
@@ -0,0 +1,21 @@
+# Solution to probem 5 #
+
+# Student: Niamh O'Leary
+# ID: G00376339 #
+
+# Write a problem that asks the user to input a postive integer and tells the user whether or not the number is a prime #
+
+# take an input from the user
+ num = int(input("Please enter a positive integer:"))
+
+ if num > 1: # prime numbers are greater than 1#
+     for i in range(2,num): #this checks if the number is divisible by any number from 2 to -1~
+         if (num % i) == 0: 
+             print(num, "This is not a prime number")
+             print(i, "times", num//i, "is", num) #A composite number must have a factor less then the square root of that number, otherwise the number is a prime number#
+             break
+        else:
+            print(num, "This is a prime number")
+
+else:
+    print(num, "This is not a prime number") # if the input number is less than or equal to 1, it is not a prime number# 
