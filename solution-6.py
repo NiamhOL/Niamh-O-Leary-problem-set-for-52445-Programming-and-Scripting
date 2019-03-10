55bb5d9e213979cef87c0dd031e00c2eddf5d366
Niamh O'Leary <47368612+NiamhOL@users.noreply.github.com>
2019-03-10 14:37:39 +0000

    Add files via upload

---
+++ solution-6.py
@@ -0,0 +1,10 @@
+# Solution to problem 6 #
+
+#Student: Niamh O'Leary#
+#ID: G00376339#
+
+#Write a program that takes a user input string and outputs every second word#
+
+string = "The quick broen fox jumps over the lazy dog"
+even_words = string.split(' ')[::2] #split the original string. Then use slice to select every second word#
+
