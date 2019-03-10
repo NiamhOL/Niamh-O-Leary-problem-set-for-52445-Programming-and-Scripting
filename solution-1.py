916c64531032056faf2d95095de72529c1c179ce
Niamh O'Leary <47368612+NiamhOL@users.noreply.github.com>
2019-03-10 15:39:09 +0000

    Update solution-1.py

--- solution-1.py
+++ solution-1.py
@@ -5,9 +5,9 @@
 
 # Write a program that asks the user to input any positive integer and outputs the sum of all numbers between one and that number
 
-n = int(input("Enter a positive number:")) 
-sum_num = (n * (n+1))/2
-print (sum_num)
+n = int(input("Enter a positive number:")) #asks user to input a numer#
+sum_num = (n * (n+1))/2 #lists all the numbers between one & the selected number. Calulates the sum of the positive intergers and repeats process#
+print (sum_num) #prints all the outputs#
 
 Print ("I just wrote my first program")
 
