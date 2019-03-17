e9567e5bb28c8ffa017e54493c338a736ed1e8ae
Niamh O'Leary <47368612+NiamhOL@users.noreply.github.com>
2019-03-17 16:06:13 +0000

    Add files via upload

--- solution-10.py
+++ solution-10.py
@@ -1,15 +1,31 @@
-# Solution to problem 10#
+# Solution to problem 10. Each plot is saved as a seperate .png file #
 
 #Student: Niamh O'Leary#
 #ID: G00376339#
 
-import matplotlib.pyplot as plt               
+import matplotlib.pyplot as plt                #import matplotlib#
                                                                                             
-plt.plot([0,1,2,3,4])                           
+plt.plot([0,1,2,3,4])                          #the function of x#
 [<matplotlib.lines.Line2D at 0x1fbbe53ddd8>]  
                                               
-plt.ylabel('numbers')                         
+plt.ylabel('numbers')                          #y axis label
 Text(0, 0.5, 'numbers')                       
                                               
 plt.show()                                    
-                            
+
+ plt.plot([0,1,2,4,6,8])                       # for the function x to the power of 2#       
+ [<matplotlib.lines.Line2D at 0x24442cbdc88>]           
+                                                        
+ plt.ylabel('numbers')                         #  y axis label#        
+ Text(0, 0.5, 'numbers')                                
+                                                        
+ plt.show()                                             
+
+ plt.plot([0,2,4,8,16])                        # for the function 2 to the power of x#
+[<matplotlib.lines.Line2D at 0x24440ddb588>]     
+                                                 
+plt.ylabel('numbers')                          # y axis label #
+Text(0, 0.5, 'numbers')                          
+                                                 
+plt.show()                                       
+                                                                             
