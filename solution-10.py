598ff7a4a048bd0b4bc9c35e05e6509109971030
Niamh O'Leary <47368612+NiamhOL@users.noreply.github.com>
2019-03-17 15:23:50 +0000

    Add files via upload

---
+++ solution-10.py
@@ -0,0 +1,15 @@
+# Solution to problem 10#
+
+#Student: Niamh O'Leary#
+#ID: G00376339#
+
+import matplotlib.pyplot as plt               
+                                                                                            
+plt.plot([0,1,2,3,4])                           
+[<matplotlib.lines.Line2D at 0x1fbbe53ddd8>]  
+                                              
+plt.ylabel('numbers')                         
+Text(0, 0.5, 'numbers')                       
+                                              
+plt.show()                                    
+                            
