ef08269414a26fc6c7e1b2832d1900e34cfee01c
Niamh O'Leary <47368612+NiamhOL@users.noreply.github.com>
2019-03-17 11:10:21 +0000

    Add files via upload

---
+++ solution-8.py
@@ -0,0 +1,21 @@
+# Solution to problem #
+
+# Student: Niamh O'Leary#
+
+#ID: GG00376339 # 
+
+# Write a program that outputs today's date and time in the format. "Monday, January 19th 2019 at 1.15pm"#
+
+import datetime
+
+now = datetime.datetime.now()  #selects todays date and time#
+
+print ("Current date and time: "
+
+print (now.strftime("%A, %B %dth %Y at %I:%M%p")) #selecting different strftime directives to generate the requested format#
+Sunday, March 17th 2019 at 10:31AM
+
+ TGREEN = "\033[32m" #change python text to green for St. Patrick's Day#
+
+In [8]: print (TGREEN + "La Feile Padraig")
+La Feile Padraig
