1dbcaf65ce857f8374295b46dfe7489587f8fdc7
Niamh O'Leary <47368612+NiamhOL@users.noreply.github.com>
2019-03-26 20:42:28 +0000

    Update solution-4.py

--- solution-4.py
+++ solution-4.py
@@ -3,19 +3,19 @@
 #Student: Niamh O'Leary#
 #ID: G00376339#
 
-def collatz(number):
+def collatz(number):  # The collatz conjecture is that, no matter what number you start with, you will allways reach 1 # 
 
-    if number % 2 ==0: #Even number#
-        print(number // 2)
+    if number % 2 ==0: # Even number #
+        print(number // 2) # number is even, divide by 2 #
         return number // 2
     
-    elif number % 2 == 1:
-        result = 3 * number + 1
+    elif number % 2 == 1:  # if the number is an odd number, 
+        result = 3 * number + 1 # multiply by 3 and subtract 1 #
         print(result)
         return result
 
-n = input("Please input a positive number:")
+n = input("Please input a positive number:")  # ask user to input a positive number # 
 while n != 1:
-     print n = collatz(int(n))
+     print n = collatz(int(n)) # prints results #
 
 
