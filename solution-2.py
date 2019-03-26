2bc90cb037c32148bcda4a0989a89236ba8a81e3
Niamh O'Leary <47368612+NiamhOL@users.noreply.github.com>
2019-03-26 20:25:43 +0000

    Update solution-2.py

--- solution-2.py
+++ solution-2.py
@@ -7,11 +7,11 @@
 import datetime
 
 
-x = datetime.datetime.now()
-print(x.strftime("%A"))
+x = datetime.datetime.now()  # using datetime function to identify the current date and time #
+print(x.strftime("%A")) # using a string function to print date and time # 
 
-if datetime.datetime.today().weekday() == 1 or datetime.datetime.today().weekday() == 3: 
-    print("Yes- today begins with a T")
+if datetime.datetime.today().weekday() == 1 or datetime.datetime.today().weekday() == 3: # assigning a number to Tues and Thurs, if this number is returned then correct #
+    print("Yes- today begins with a T") # if day is equal to 1 or 3 then the dat starts with a T. Therefore print TOday begins with a T #
 else:                                       
-    print("No - Today does not begin with a T") 
-
+    print("No - Today does not begin with a T") # otherwise the day does not start with a T and print same #
+# for description of method and references please see accompying README file in GITHUB repository # 
